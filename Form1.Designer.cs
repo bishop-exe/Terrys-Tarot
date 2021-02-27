0b8518bed22ae6a47add754f95b49520071dab56
bishop-exe <athf.flcl@gmail.com>
2021-02-27 12:44:36 -0500

    Terry's Tarot V2 initial push

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Terry_sTarotV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.PanWest;
+            this.textBox1.Font = new System.Drawing.Font("NSimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.textBox1.Location = new System.Drawing.Point(281, 255);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(530, 141);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "/ / / / / All interpretations were dirved from the work of A. E. Waite and his at" +
+    "tempt of a systematic study of the history of occultism./ / / / /";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Image = global::Terry_sTarotV2.Properties.Resources.button;
+            this.pictureBox3.Location = new System.Drawing.Point(292, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(495, 145);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Terry_sTarotV2.Properties.Resources.border;
+            this.pictureBox2.InitialImage = global::Terry_sTarotV2.Properties.Resources.border;
+            this.pictureBox2.Location = new System.Drawing.Point(252, 244);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(590, 169);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Terry_sTarotV2.Properties.Resources.bg;
+            this.pictureBox1.InitialImage = global::Terry_sTarotV2.Properties.Resources._0_Fool;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(267, 537);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(589, 551);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 11);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "This Abomination Was Created by BISHOP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 11F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(834, 560);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "   /  /  /  /  /  / T e r r y \' s   T a r o t  /   /  /  /  /  /  /  /";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Terry_sTarotV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Select(0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private Bitmap[] GetResourceImages()
+        {
+
+            PropertyInfo[] props = typeof(Properties.Resources).GetProperties(BindingFlags.NonPublic | BindingFlags.Static);
+            var images = props.Where(prop => prop.PropertyType == typeof(Bitmap)).Select(prop => prop.GetValue(null, null) as Bitmap).ToArray();
+            return images;
+        }
+        private void RandomizePicture()
+        {
+            Bitmap[] images = GetResourceImages();
+            if (images == null || images.Length == 0)
+            {
+                //Nothing to do here...
+                return;
+            }
+
+            int maxValue = images.Length;
+            DateTime moment = DateTime.Today;
+            
+            //Random r = new Random(moment.Day);
+            string name_seed = Environment.UserName.ToString();
+            int numVal = Convert.ToInt32(name_seed[0]);
+            Random r = new Random(moment.Day + numVal);
+            bool rotate = false;
+            int idx = r.Next(maxValue);
+            while (idx > 21)
+            {
+                idx = r.Next(maxValue);
+            }
+            this.pictureBox1.Image = images[idx];
+            if (r.Next(0, 2) == 0) {
+                Console.WriteLine("Rotate");
+                rotate = true;
+            }
+            switch (idx)
+            {
+                case 0:
+                    if (!rotate) {
+                        textBox1.Text = "0.THE FOOL--Folly, mania, extravagance, intoxication, delirium, frenzy, bewrayment. ";
+                    }
+                    else
+                        textBox1.Text = "0.THE FOOL--Reversed: Negligence, absence, distribution, carelessness, apathy, nullity, vanity.";
+                    break;
+                case 1:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "1.MAGICIAN--Skill, diplomacy, address, subtlety, pain, loss, disaster, snares of enemies; self-confidence, will; [it signifies] the Querent, if male.";
+                    }
+                    else
+                        textBox1.Text = "1.MAGICIAN--Reversed: Physician, mental disease, disgrace, disquiet.";
+                    break;
+                case 2:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "10.THE WHEEL--Destiny, fortune, success, elevation, luck, felicity.";
+                    }
+                    else
+                        textBox1.Text = "10.THE WHEEL--Reversed:  Increase, abundance, superfluity.";
+                    break;
+                case 3:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "11.JUSTICE--Equity, rightness, probity, executive; triumph of the deserving side in law.";
+                    }
+                    else
+                        textBox1.Text = "11.JUSTICE--Reversed: Law in all its departments, legal complications, bigotry, bias, excessive severity.";
+                    break;
+                case 4:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "12. THE HANGED MAN --Wisdom, circumspection, discernment, trials, sacrifice, intuition, divination, prophecy.";
+                    }
+                    else
+                        textBox1.Text = "12. THE HANGED MAN --Reversed: Selfishness, the crowd, body politic.";
+                    break;
+                case 5:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "13.DEATH--End, mortality, destruction, corruption; also, for a man, the loss of a benefactor; for a woman, many contrarieties; for a maid, failure of marriage projects";
+                    }
+                    else
+                        textBox1.Text = "13.DEATH--Reversed: Inertia, sleep, lethargy, petrifaction, somnambulism; hope destroyed.";
+                    break;
+                case 6:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "14.TEMPERANCE--Economy, moderation, frugality, management, accommodation. ";
+                    }
+                    else
+                        textBox1.Text = "14.TEMPERANCE--Reversed: Things connected with churches, religions, sects, the priesthood, sometimes even the priest who will marry Querent; also disunion, unfortunate combinations, competing interests.";
+                    break;
+                case 7:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "15.THE DEVIL--Ravage, violence, vehemence, extraordinary efforts, force, fatality; that which is predestined but is not for this reason evil. ";
+                    }
+                    else
+                        textBox1.Text = "15.THE DEVIL--Reversed: Evil fatality, weakness, pettiness, blindness.";
+                    break;
+                case 8:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "16. THE TOWER-- Misery, distress, indigence, adversity, calamity, disgrace, deception, ruin. It is a card in particular of unforeseen catastrophe.";
+                    }
+                    else
+                        textBox1.Text = "16. THE TOWER--Reversed: Negligence, absence, distribution, carelessness, apathy, nullity, vanity.";
+                    break;
+                case 9:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "17.THE STAR--Loss, theft, privation, abandonment; another reading says--hope bright prospects,";
+                    }
+                    else
+                        textBox1.Text = "17.THE STAR--Reversed: Arrogance, haughtiness, impotence.";
+                    break;
+                case 10:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "18.THE MOON--Hidden enemies, danger, calumny, darkness, terror, deception, occult forces, error. ";
+                    }
+                    else
+                        textBox1.Text = "18.THE MOON--Reversed: Instability, inconstancy, silence, lesser degrees of deception and error.";
+                    break;
+                case 11:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "19.THE SUN--Enlightment, Joy, Marriage, Material happiness, Success, Vitality.";
+                    }
+                    else
+                        textBox1.Text = "19.THE SUN--Reversed: False impressions, Lack of clarity, Low Vitality, Sadness";
+                    break;
+                case 12:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "2.THE HIGH PRIESTESS--Secrets, mystery, the future as yet unrevealed; the woman who interests the Querent, if male; the Querent herself, if female; silence, tenacity; mystery, wisdom, science.";
+                    }
+                    else
+                        textBox1.Text = "2.THE HIGH PRIESTESS--Reversed: Passion, moral or physical ardor, conceit, surface knowledge.";
+                    break;
+                case 13:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "20.JUDGEMENT--Awakening, Decision making, Redemption, Reincarnation, Renewal, Transition.";
+                    }
+                    else
+                        textBox1.Text = "20.JUDGEMENT--Reversed: Poor logic, Poor or hasty judgement, Self-doubt, Stagnation.";
+                    break;
+                case 14:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "21.THE WORLD—-Assured success, recompense, voyage, route, emigration, flight, change of place.";
+                    }
+                    else
+                        textBox1.Text = "21.THE WORLD—-Reversed: Inertia, fixity, stagnation, permanence.";
+                    break;
+                case 15:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "3. THE EMPRESS--Fruitfulness, action, initiative, length of days; the unknown, clandestine; also difficulty, doubt, ignorance.";
+                    }
+                    else
+                        textBox1.Text = "3. THE EMPRESS--Reversed: Light, truth, the unraveling of involved matters, public rejoicings; according to another reading, vacillation.";
+                    break;
+                case 16:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "4. THE EMPEROR--Stability, power, protection, realization; a great person; aid, reason, conviction also authority and will";
+                    }
+                    else
+                        textBox1.Text = "4. THE EMPEROR-- Reversed: Benevolence, compassion, credit; also confusion to enemies, obstruction, immaturity. ";
+                    break;
+                case 17:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "5. THE HIEROPHANT--Marriage, alliance, captivity, servitude; by another account, mercy, and goodness; inspiration; the man to whom the Querent has recourse.";
+                    }
+                    else
+                        textBox1.Text = "5. THE HIEROPHANT--Reversed: Society, good understanding, concord, over kindness, weakness.";
+                    break;
+                case 18:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "5. THE LOVERS--Attraction, love, beauty, trials overcome.";
+                    }
+                    else
+                        textBox1.Text = "5. THE LOVERS--Reversed: Failure, foolish designs. Another account speaks of marriage frustrated and contrarieties of all kinds.";
+                    break;
+                case 19:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "7. THE CHARIOT--Succour, providence; also war, triumph, presumption, vengeance, trouble.";
+                    }
+                    else
+                        textBox1.Text = "7. THE CHARIOT--Reversed: Riot, quarrel, dispute, litigation, defeat.";
+                    break;
+                case 20:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "8. STRENGTH--Power, energy, action, courage, magnanimity; also complete success and honours.";
+                    }
+                    else
+                        textBox1.Text = "8. STRENGTH--Reversed: Despotism, abuse of power, weakness, discord, sometimes even disgrace.";
+                    break;
+                case 21:
+                    if (!rotate)
+                    {
+                        textBox1.Text = "9.THE HERMIT--Prudence, circumspection; also and especially treason, dissimulation, roguery, corruption. ";
+                    }
+                    else
+                        textBox1.Text = "9.THE HERMIT--Reversed: Concealment, disguise, policy fear, unreasoned caution.";
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.RandomizePicture();
+        }
+    }
+}
